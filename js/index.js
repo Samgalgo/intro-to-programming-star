88f0b546e8017438ef139010260e363c00068d82
Samantha Galindo <bluesundaysg@gmail.com>
2021-12-22 14:39:57 -0500

    Fetch API to my github projects, Add style and final details to the whole page

--- js/index.js
+++ js/index.js
@@ -7,23 +7,18 @@ thisYear;
 const footer = document.querySelector("footer");
 
 //Create a new paragraph (p) element and store it in a variable named copyright
-//hint: createElement method
 const copyright = document.createElement("p");
+copyright.className="copyrights"
 
 //Set the inner HTML of your copyright element to display your name and the current year
-//hint: use thisYear variable from earlier
-
-copyright.innerHTML = `<p> Samantha Galindo &copy;<p> ${thisYear}`;
+copyright.innerHTML = `<p> &copy; Samantha Galindo   </p> ${thisYear}`;
 
 //Using "DOM Manipulation", append the copyright element to the footer
-//hint: appendChild method
 footer.appendChild(copyright);
 
 //Create List of Skills
-//Open your index.js file
-//List your technical skills by creating an Array of String values and store it in a variable named skills
-
 const skills = ["GitHub", "HTML", "JavaScript", "CSS"];
+
 //Using "DOM Selection", select the #skills section by id and store it in a variable named skillsSection
 //hint: querySelector or getElementById method
 skillsSection = document.getElementById("skills");
@@ -56,7 +51,7 @@ skillsList = skillsSection.querySelector("ul");
       //console.log(messageList);
       const newMessage = document.createElement("li");
       messageList.appendChild(newMessage);
-      newMessage.innerHTML=  ` <a href=mailto:${emails} >     ${names} </a> wrote:  ${messagesInput}`; 
+      newMessage.innerHTML=  ` <a  class="msgComing" href=mailto:${emails} >     ${names} </a> wrote:  ${messagesInput}`; 
       newMessage.className="messageArrived";
       //newMessage.textContent=messagesInput;
       console.log(newMessage);
@@ -97,47 +92,26 @@ skillsList = skillsSection.querySelector("ul");
 
    });
 
+//  Chain a then method to your fetch call and pass it a function that returns the response JSON data
+//  Chain another then method and pass it a function, inside of which you can paste the code from your previous "load" event listener function
+//  Delete the XmlHttpRequest code from the previous lesson, including the event listener
+//  Save and refresh your browser
+// You should see the same list of repositories displayed on your webpage
+   fetch("https://api.github.com/users/Samgalgo/repos")
+   .then(res => res.json())
+   .then(data => {
+      let projectSection = document.getElementById("projects");
+      let projectList = projectSection.querySelector("ul");
+     
+      for (let i = 0; i < data.length; i++) {
+        console.log(data[i]);
+        let project = document.createElement("li");
+        project.className="projectos";
+        project.innerHTML = `<a class="linkRepos" href="${data[i].html_url}" target="_blank"> ${data[i].name} </a>`;
+        projectList.appendChild(project);
+        
+      }});
    
-  
-   /* Stretch Goals
-These tasks are entirely optional, but if you'd like a challenge then do your best to complete each item.
-
- (Optional) Hide the #messages section when the list is empty
- (Optional) Create an "edit" button for each message entry that allows the user to input a new/modified message*/
-
-  //function resetValues() {
-   //document.querySelector('[name="leave_message"]').reset()
-  // }
- //resetValues()
 
 
-//  Create a new XMLHttpRequest object and store it in a variable named githubRequest, add Project section
-const githubRequest = new XMLHttpRequest();
-githubRequest.open("GET", "https://api.github.com/users/Samgalgo/repos");
-githubRequest.send();
 
-githubRequest.addEventListener('load', (event) => {
-   console.log('page is fully loaded');
-   const responses = JSON.parse(githubRequest.responseText);
-   const projectSection = document.getElementById("projects");
-   const projectList = projectSection.querySelector("ul");
-  
-   for (let i = 0; i < responses.length; i++) {
-     console.log(responses[i]);
-     project = document.createElement("li");
-     project.className="projectos";
-     project.innerText = responses[i].name;
-     projectList.appendChild(project);
-   }
-
-
- });
-
-
-//How to link my GITHUB PROJECT NAMES TO THEIR RESPECTIVE URLs
-// for (let i = 0; i < responses.length; i++) {
-//    console.log(responses[i]);
-//    projectLink = document.createElement("a");
-//    projectLink.className="projectosLink";
-//    projectLink.innerText = responses[i].html_url;
-//  }
